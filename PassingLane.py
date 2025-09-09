6a5521f069a5c23b98520d14ab483ec7d948cc7a
Z Dunson <123149208+ztdnzunYT@users.noreply.github.com>
2025-09-09 15:34:15 -0500

    Update PassingLane.py
    Added background scroll delay

--- PassingLane.py
+++ PassingLane.py
@@ -18,12 +18,14 @@ class Globals:
     START_Y = 400
     WHITE_COLOR = (255,255,255)
     BLACK_COLOR = (0,0,0)
-    GAME_STATE = "Loading_screen"
+    GAME_STATE = "loading_screen"
     delta_time = clock.get_time() / 1000
 
 class LoadingScreen:
 
     NUM_OF_SQUARES = 15 
+    TIME_DELAY = 25
+    time = pygame.time.get_ticks()
     parallax_squares = []
 
     class ParallaxSquare():
@@ -34,38 +36,38 @@ class LoadingScreen:
             self.image = pygame.transform.smoothscale((pygame.image.load(image).convert_alpha()),(200,200))
             self.rect = self.image.get_rect(topleft=(start_x,start_y))
 
-
 class MainMenu:
+    
+    @staticmethod
+    def CreateSquares():
+        for i in range(5):
+            for j in range(4):
+                new_square = LoadingScreen.ParallaxSquare((i*200)-200,(j*200)-200,"assets//Passing lane background scroll.png")
+                LoadingScreen.parallax_squares.append(new_square)
 
-    for i in range(5):
-        for j in range(4):
-            new_square = LoadingScreen.ParallaxSquare((i*200)-200,(j*200)-200,"assets//Passing lane background scroll.png")
-            LoadingScreen.parallax_squares.append(new_square)
-
-
-
-    print(LoadingScreen.parallax_squares)
+    CreateSquares()
 
     def drawBackgroundDisplay():
         for square in LoadingScreen.parallax_squares:
             SCREEN.blit(square.image,square.rect)
 
     def playBackgroundDisplay():
-        for square in LoadingScreen.parallax_squares:
-            if square.rect.x > SCREEN_WIDTH:
-                square.rect.x = -200
-            else:
-                square.rect.x += 1
+        current_time = pygame.time.get_ticks()
 
-            if square.rect.y > SCREEN_HEIGHT:
-                square.rect.y = -200
-            else:
-                square.rect.y +=1
+        if current_time > LoadingScreen.time:
+            
+            for square in LoadingScreen.parallax_squares:
+                if square.rect.x > SCREEN_WIDTH:
+                    square.rect.x = -200
+                else:
+                    square.rect.x += 1
 
+                if square.rect.y > SCREEN_HEIGHT:
+                    square.rect.y = -200
+                else:
+                    square.rect.y +=1
 
-
-
-
+            LoadingScreen.time = current_time + LoadingScreen.TIME_DELAY
 
 def fps_counter():
     fps = str(int(clock.get_fps()))
@@ -83,16 +85,10 @@ run = True
 while run:
     SCREEN.fill(Globals.BLACK_COLOR)
     clock.tick(Globals.TARGET_FPS)
-    #SCREEN.blit(parking_lot,parking_lot_rect)
 
-
-
-    MainMenu.drawBackgroundDisplay()
-    MainMenu.playBackgroundDisplay()
-
-
-
-
+    if Globals.GAME_STATE == "loading_screen":
+        MainMenu.drawBackgroundDisplay()
+        MainMenu.playBackgroundDisplay()
 
 
     for event in pygame.event.get():
@@ -114,4 +110,3 @@ while run:
 
     
 pygame.quit()
-
